9023ecc2a98dec6a67881fc6b50469b388df0440
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-07-08 22:27:11 +0530

    style : added icons

--- src/Components/RestaurantReview.js
+++ src/Components/RestaurantReview.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StarOutline } from 'react-ionicons'
 
 const RestaurantReview = ({item}) => {
   const {pp,revName, comment, rating} = item
@@ -15,7 +16,13 @@ const RestaurantReview = ({item}) => {
           />
           <h3>{revName}</h3>
         </div>
-        <div className="review-card"><h2>{rating}</h2></div>
+        <div className="review-card"><h2>{rating}
+<StarOutline
+  color={'#FFD601'} 
+  height="20px"
+  width="20px"
+/>
+</h2></div>
       </div>
       <p className="review-content">{comment}</p>
     </div>
